8fea5c7b6e4a13d5bc6fd9a64eef8cdf42b9663e
fanne <896661380@qq.com>
2017-08-23 10:35:37 +0800

    4.3,初始化scrapy项目

---
+++ AritcleSpider/main.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+__Author__ = '7zGame Fanne'
+__Date__ = '2017/8/22 16:59'
+
+
+import sys
+import os
+from scrapy.cmdline import execute
+# 调用execute可以执行scrapy脚本
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "jobbole"])
+
