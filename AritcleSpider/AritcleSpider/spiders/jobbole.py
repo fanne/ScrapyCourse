f0a9bf361c6d4046a45d9d233ffcec4338352417
fanne <896661380@qq.com>
2017-08-25 10:08:16 +0800

    4.9:xpath,css选择器使用

--- AritcleSpider/AritcleSpider/spiders/jobbole.py
+++ AritcleSpider/AritcleSpider/spiders/jobbole.py
@@ -1,11 +1,92 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+from scrapy.http import Request
+from urllib import parse
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
+        """
+        1、获取文章列表页中的文章url彬交给解析函数进行具体字段的解析
+        2、获取下一页的url并交给scrapy下载
+        """
+
+        # 解析列表页中所有文章的url并交给scrapy下载后并进行解析
+        post_urls = response.css("#archive .floated-thumb .post-meta a::attr(href)").extract()
+        for post_url in post_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+
+        # 提交下一页并提交给scrapy下
+        next_urls = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse)
+
+
+    def parse_detail(self, response):
+        """
+        提前文章具体字段逻辑
+        :param response:
+        :return:
+        """
+        # 使用xpath获取值
+        # firfox返回
+        re1_selector = response.xpath('/html/body/div[3]/div[3]/div[1]/div[1]/h1')
+        # chrome返回
+        re2_selector = response.xpath('//*[@ id = "post-110287"] / div[1] / h1')
+        # title = response.xpath('//*[@class="entry-header"]/h1/text()').extract_first()
+        # # extract_first可以不用对数组取值做异常处理，用数组下标可能出在异常情况
+        # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·","").strip()
+        # praise_nums = response.xpath("//span[contains(@class,'vote-post-up')]/h10/text()").extract()[0]
+        # fav_nums = response.xpath("//span[contains(@class,'bookmark-btn')]/text()").extract()[0]
+        # match_re = re.match(r".*(\d+).*",fav_nums)
+        # if match_re:
+        #     fav_nums = int(match_re.group(1))
+        # else:
+        #     fav_nums = 0
+        # comment_nums = response.xpath("//a[@href='#article-comment']/span").extract()[0]
+        # match_re = re.match(r".*(\d+).*", comment_nums)
+        # if match_re:
+        #     comment_nums = int(match_re.group(1))
+        # else:
+        #     comment_nums = 0
+        # content = response.xpath("//div[@class='entry']").extract()[0]
+        # tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        # tags = ",".join(tag_list)
+        #
+        # print(title)
+        # print(create_date)
+        # print(praise_nums)
+        # print(fav_nums)
+        # print(comment_nums)
+        # print(content)
+        # print(tags)
+
+
+        # 使用css选择器获取值
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(r".*(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+        content = response.css("div.entry").extract()[0]
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
         pass
